c176d81998f5dd61cf6ae1daef6e558613df5409
youz2me <kynhun20@gachon.ac.kr>
2024-03-06 14:09:33 +0900

    Design: #3 - OnboardingView 배경 적용

--- Honzapda/Presentation/Onboarding/OnboardingView.swift
+++ Honzapda/Presentation/Onboarding/OnboardingView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("background_onboarding")
+                .resizable()
+            VStack {
+            }
+        }
     }
 }
 
