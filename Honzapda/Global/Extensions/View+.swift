fb93af0369281164475a72dd362d481ca4e4a68c
youz2me <kynhun20@gachon.ac.kr>
2024-03-12 13:47:29 +0900

    Feat: #3 - Extension에 텍스트 부분 하이라이트 기능 구현한 HighlightedText 선언

---
+++ Honzapda/Global/Extensions/View+.swift
@@ -0,0 +1,81 @@
+//
+//  View+.swift
+//  Honzapda
+//
+//  Created by YOUJIM on 3/12/24.
+//
+
+import SwiftUI
+
+
+// MARK: 문장 중간 지정된 단어들의 색깔과 굵기를 바꿔주는 구조체 선언
+// CAUTION:  처음 등장하는 단어를 체크하기 때문에 공통된 단어들일지라도 처음 등장한 단어에만 적용됨
+// 모두 적용하고 싶으면 원하는 단어의 수대로 적용해줘야함
+
+struct MultiColoredText: View {
+    var originalText: String
+    var highlightedSubstrings: [(String, Color)]
+    var highlightedFont: Font
+    var originalColor: Color
+    var originalFont: Font
+    
+    var body: some View {
+        var currentIndex = originalText.startIndex
+        var result: Text = Text("")
+        
+        for (substring, color) in highlightedSubstrings {
+            if let range = originalText.range(
+                of: substring,
+                range: currentIndex ..< originalText.endIndex
+            ) {
+                let beforeRange = originalText[currentIndex ..< range.lowerBound]
+                let coloredText = originalText[range]
+                
+                result = (result + Text(beforeRange))
+                    .foregroundColor(originalColor)
+                    .font(originalFont)
+                result = (result + Text(coloredText))
+                    .foregroundColor(color)
+                    .font(highlightedFont)
+                
+                currentIndex = range.upperBound
+            }
+        }
+        
+        let remainingText = originalText[currentIndex...]
+        result = (result + Text(remainingText))
+            .foregroundColor(originalColor)
+        
+        return result
+    }
+}
+
+struct TextWithColoredSubstring: View {
+    var originalText: String
+    var coloredSubstring: String
+    var originalColor: Color
+    var originalFont: Font
+    var highlightedColor: Color
+    var highlightedFont: Font
+    
+    var body: some View {
+        if let coloredRange = originalText.range(of: coloredSubstring) {
+            let beforeRange = originalText[..<coloredRange.lowerBound]
+            let coloredText = originalText[coloredRange]
+            let afterRange = originalText[coloredRange.upperBound...]
+            
+            return Text(beforeRange)
+                .foregroundColor(originalColor)
+                .font(originalFont)
+            + Text(coloredText)
+                .foregroundColor(highlightedColor)
+                .font(highlightedFont)
+            + Text(afterRange)
+                .foregroundColor(originalColor)
+                .font(originalFont)
+        } else {
+            return Text(originalText)
+                .foregroundColor(.black)
+        }
+    }
+}
