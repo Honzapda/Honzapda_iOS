8fe341c283d185256e1ceec937ccb0176d33ab01
0Hooni <thddudgns972@gmail.com>
2024-04-05 21:41:41 +0100

    Chore: #15 - MVVM 패턴 적용
    - Model, VM, View로 나누어 기존 코드 분리
    - View를 재사용 가능한 View와 일반 View로 나눔

---
+++ Honzapda/Presentation/Cafe/View/Main/MoreReviewView.swift
@@ -0,0 +1,19 @@
+//
+//  MoreReview.swift
+//  Honzapda
+//
+//  Created by 송영훈 on 4/1/24.
+//
+
+import SwiftUI
+
+struct MoreReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    // TODO: Navibar right button
+}
+
+#Preview {
+    MoreReviewView()
+}
