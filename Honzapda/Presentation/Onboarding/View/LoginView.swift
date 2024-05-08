1bdc24b845646a2374f7952ee40167957e60118f
youz2me <kynhun20@gachon.ac.kr>
2024-05-08 20:45:20 +0900

    Add: #29 - 로그인 배경 에셋 추가

---
+++ Honzapda/Presentation/Onboarding/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Honzapda
+//
+//  Created by YOUJIM on 5/8/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
