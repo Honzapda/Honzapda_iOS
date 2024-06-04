20ab2ee1dfce0fd385caafe8571eb60bdf8f15b6
0Hooni <thddudgns972@gmail.com>
2024-06-04 18:08:57 +0900

    feat: #47 - 회원가입 후 큐레이션 이동
    - DispatchQueue를 이용하여 View 이동 구현

--- Honzapda/Presentation/Onboarding/View/Main/SignUpCompleteView.swift
+++ Honzapda/Presentation/Onboarding/View/Main/SignUpCompleteView.swift
@@ -8,29 +8,43 @@
 import SwiftUI
 
 struct SignUpCompleteView: View {
+    @State private var isTimerFinish = false
+    
     var body: some View {
-        VStack {
-            Text("HONZAPDA")
-                .font(.sCoreDream(.bold, size: 22))
-                .foregroundStyle(.primary05)
-                .padding(.bottom, 2)
-            Text("가입을 축하해요!")
-                .font(.sCoreDream(.medium, size: 22))
-                .foregroundStyle(.gray10)
-                .padding(.bottom, 16)
-            Text("어느 카페 가지? 자리가 있을까?\n더 이상 고민하지 마세요.")
-                .multilineTextAlignment(.center)
-                .font(.sCoreDream(.medium, size: 14))
-                .lineSpacing(6.0)
-                .foregroundStyle(.gray06)
-                .padding(.bottom, 32)
-            Image(.iconSplashLogo)
+        NavigationView {
+            VStack {
+                Text("HONZAPDA")
+                    .font(.sCoreDream(.bold, size: 22))
+                    .foregroundStyle(.primary05)
+                    .padding(.bottom, 2)
+                Text("가입을 축하해요!")
+                    .font(.sCoreDream(.medium, size: 22))
+                    .foregroundStyle(.gray10)
+                    .padding(.bottom, 16)
+                Text("어느 카페 가지? 자리가 있을까?\n더 이상 고민하지 마세요.")
+                    .multilineTextAlignment(.center)
+                    .font(.sCoreDream(.medium, size: 14))
+                    .lineSpacing(6.0)
+                    .foregroundStyle(.gray06)
+                    .padding(.bottom, 32)
+                Image(.iconSplashLogo)
+                
+                NavigationLink(
+                    destination: SignUpCurationView(),
+                    isActive: $isTimerFinish,
+                    label: { EmptyView() })
+            }
         }
         .background(
             Image(.backgroundSignupComplete)
                 .ignoresSafeArea(.all)
                 .aspectRatio(contentMode: .fit)
         )
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.isTimerFinish = true
+            }
+        }
     }
 }
 
